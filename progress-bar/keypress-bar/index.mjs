40b8aed98f5447f73b51e03b348f516a067b236a
Your name here <isaacxpreston7@gmail.com>
2021-09-28 12:02:11 -0400

    more keyboard events on progress bar

--- progress-bar/keypress-bar/index.mjs
+++ progress-bar/keypress-bar/index.mjs
@@ -6,35 +6,44 @@ import fs from 'fs'
 import readline from 'readline'
 import open from 'open'
 import Progress from 'progress'
-
-// download file from url
 const fileName = path.resolve(path.resolve(), 'myVideo.mp4')
 const destination = fs.createWriteStream(fileName)
+
 const req = https.request({
   host: 'file-examples-com.github.io',
   path: 'uploads/2017/04/file_example_MP4_640_3MG.mp4'
 })
 
-// cancel download on 's' key
-readline.emitKeypressEvents(process.stdin)
-process.stdin.setRawMode(true)
-process.stdin.on('keypress', function (str, key) {
+const msg = {
+  cancel: '\ndownload cancelled',
+  sigint: '\ndownload interrupted'
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.input.on('keypress', function (str, key) {
   if (str === 's') {
-    console.log('\ndownload cancelled')
-    process.stdin.setRawMode(false)
+    console.log(msg.cancel)
     fs.rmSync(fileName)
     process.exit(1)
   }
 })
 
-req.on('response', function (res) {
-  const len = parseInt(res.headers['content-length'], 10)
+rl.on('SIGINT', () => {
+  console.log(msg.sigint)
+  fs.rmSync(fileName)
+  process.exit(1)
+})
 
+req.on('response', function (res) {
   const bar = new Progress('[:bar] :rate/bps :percent :etas', {
     complete: '#',
     incomplete: '_',
     width: Math.floor(process.stdout.columns / 3),
-    total: len
+    total: parseInt(res.headers['content-length'], 10)
   })
 
   res.pipe(destination)
@@ -45,6 +54,7 @@ req.on('response', function (res) {
 
   res.on('end', async function () {
     console.log('download complete')
+    msg.cancel = msg.sigint = ''
     await open(fileName, { wait: true })
     fs.rmSync(fileName)
     process.exit(0)
@@ -53,4 +63,4 @@ req.on('response', function (res) {
 
 console.log(`download starting
 press 's' to cancel`)
-const foo = req.end()
+req.end()
