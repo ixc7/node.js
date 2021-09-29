0f9cdb46975c110d37100b36ead4415bd36e6590
Your name here <isaacxpreston7@gmail.com>
2021-09-29 18:25:35 -0400

    fixed example

--- centered-text/index.mjs
+++ centered-text/index.mjs
@@ -1,12 +1,13 @@
 import readline from 'readline'
 
-const msg = 'hello world'
-const x = Math.floor((process.stdout.columns / 2) - msg.length)
+const input = process.argv.slice(2).join(' ')
+const msg = input.length < 1 ? 'hello world' : input
+const x = Math.floor((process.stdout.columns / 2) - (msg.length / 2))
 const y = Math.floor((process.stdout.rows / 2) - 1)
 
 console.clear()
 
-readline.cursorTo(process.stdout, x, y, function() {
+readline.cursorTo(process.stdout, x, y, function () {
   process.stdin.write(msg)
   readline.cursorTo(process.stdout, 0, process.stdout.rows)
 })
